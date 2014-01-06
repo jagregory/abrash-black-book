ba650ed5b975f772afc31fb024c00c9b7c89f0b6
James Gregory <james@jagregory.com>
2014-01-06 22:17:21 +1100

    Include all the chapters in the epub

--- make-epub.sh
+++ make-epub.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 pandoc -S -o main.epub index.md intro.md about.md about_author.md \
- 01.md 02.md 03.md 04.md 05.md 06.md 07.md 08.md 09.md 10.md
+ 01.md 02.md 03.md 04.md 05.md 06.md 07.md 08.md 09.md 10.md \
+ 11.md 12.md 13.md 14.md 15.md 16.md 17.md 18.md 19.md 20.md \
+ 21.md 22.md 23.md 24.md 25.md 26.md 27.md 28.md 29.md 30.md \
+ 31.md 32.md 33.md 34.md 35.md 36.md 37.md 38.md 39.md 40.md \
+ 41.md 42.md 43.md 44.md 45.md 46.md 47.md 48.md 49.md 50.md \
+ 51.md 52.md 53.md 54.md 55.md 56.md 57.md 58.md 59.md 60.md \
+ 61.md 62.md 63.md 64.md 65.md 66.md 67.md 68.md 69.md 70.md \
+ appendix-a.md
